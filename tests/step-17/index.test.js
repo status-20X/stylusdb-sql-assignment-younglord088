e46620c4da429b94c309a289cdbb68d76b8cbfe7
Yash Panjwani <yashmpanjwani.com>
2024-05-05 12:48:47 +0530

    changes in 16

--- tests/step-17/index.test.js
+++ tests/step-17/index.test.js
@@ -1,6 +1,6 @@
 const {readCSV} = require('../../src/csvReader');
-const {executeSELECTQuery } = require('../../src/index');
-const { parseJoinClause, parseSelectQuery } = require('../../src/queryParser');
+const {parseSelectQuery, parseJoinClause} = require('../../src/queryParser');
+const {executeSELECTQuery} = require('../../src/index');
 
 test('Read CSV File', async () => {
     const data = await readCSV('./student.csv');
